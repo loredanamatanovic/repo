f175e465eb035cad1f917e0e0ee510f3df8c6093
loredanamatanovic <loredanamatanovic@gmail.com>
2025-03-13 19:09:44 +0100

    Ispravan kod

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,8 @@
 public class Main {
-    public Main() {
-    }
-
     public static void main(String[] args) {
         int sum = 0;
 
-        for(int i = 1; i < 100; ++i) {
+        for (int i = 1; i < 100; ++i) {
             if (i % 2 == 0) {
                 sum += i;
             }
